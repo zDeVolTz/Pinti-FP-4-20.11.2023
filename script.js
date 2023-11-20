3df8bc7a9b0a8a2c185c331905a419c12fd9b122
zDeVolTz <vlad.pinti@gmail.com>
2023-11-20 07:43:53 +0200

    create script

---
+++ script.js
@@ -0,0 +1,11 @@
+const NumberFirst = +prompt("Введіть перше число");
+const NumberSecond = +prompt("Введіть друге число");
+const SumResult = NumberFirst + NumberSecond;
+const SubtracResult = NumberFirst - NumberSecond;
+const MultiplicatResult = NumberFirst * NumberSecond;
+const DivisionResult = NumberFirst / NumberSecond;
+
+alert(`${NumberFirst}+${NumberSecond}=${SumResult}\n${NumberFirst}-${NumberSecond}=${SubtracResult}\n${NumberFirst}*${NumberSecond}=${MultiplicatResult}\n${NumberFirst}/${NumberSecond}=${DivisionResult}`);
+
+
+
